14bd7ad3238ce70add0bfd1af8ecd1d60dc0e576
Anjain <ankur.jain@ongraph.com>
2014-06-13 18:53:19 +0530

    code deploy

--- config/application.rb
+++ config/application.rb
@@ -28,14 +28,14 @@ module Xerofit
       # STRIPE_API_KEY = "sk_test_CSZ1ZaPwKkUnqoy9CRHVOaBA"
       # STRIPE_PUB_KEY = "pk_test_tPb28bRAb7DWYFpeU9l8oKhB"
 
-      FACEBOOK_KEY = "1441124259474236"
-      FACEBOOK_SECRET = "93b2c340b13ed77606c4d2b5129b9381"
+      # FACEBOOK_KEY = "1441124259474236"
+      # FACEBOOK_SECRET = "93b2c340b13ed77606c4d2b5129b9381"
 
-      APP_ID = "1044996955605-gr0lvpu3ejbf4jlc2ghh3ut3vt78h8er.apps.googleusercontent.com"
-      APP_SECRET_ID = "EiJx0W5quLsbidRx-n2Y6AyE"
+      # APP_ID = "1044996955605-gr0lvpu3ejbf4jlc2ghh3ut3vt78h8er.apps.googleusercontent.com"
+      # APP_SECRET_ID = "EiJx0W5quLsbidRx-n2Y6AyE"
 
-      TWITTER_KEY = "OfWiBajwBXc5SMdR5fDxfR6O7"
-      TWITTER_SECRET = "ig8B2noq4szAMMKAuzn37ssT98bB7ZDRPZy8FeFCy0K6zSxbEz"
+      # TWITTER_KEY = "OfWiBajwBXc5SMdR5fDxfR6O7"
+      # TWITTER_SECRET = "ig8B2noq4szAMMKAuzn37ssT98bB7ZDRPZy8FeFCy0K6zSxbEz"
 
       config.action_mailer.delivery_method = :smtp
         ActionMailer::Base.smtp_settings = {
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 require "omniauth-facebook"
 Rails.application.config.middleware.use OmniAuth::Builder do
   
-  provider :facebook, FACEBOOK_KEY, FACEBOOK_SECRET, :scope => 'email,read_stream', :display => 'popup'
-  provider :google_oauth2, APP_ID, APP_SECRET_ID, scope: 'userinfo.email, userinfo.profile'
-  provider :twitter, TWITTER_KEY, TWITTER_SECRET
+  provider :facebook, "1441124259474236", "93b2c340b13ed77606c4d2b5129b9381", :scope => 'email,read_stream', :display => 'popup'
+  provider :google_oauth2, "1044996955605-gr0lvpu3ejbf4jlc2ghh3ut3vt78h8er.apps.googleusercontent.com", "EiJx0W5quLsbidRx-n2Y6AyE", scope: 'userinfo.email, userinfo.profile'
+  provider :twitter, "OfWiBajwBXc5SMdR5fDxfR6O7", "ig8B2noq4szAMMKAuzn37ssT98bB7ZDRPZy8FeFCy0K6zSxbEz"
 end
