5e491af3657536b6cff90a7b8eab243c0b9a9b6f
vishwnath <vishwnath.yadav@ongraph.com>
2014-06-27 03:04:17 -0700

    added dashboard file

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,8 +1,8 @@
 class Admin::AdminController < ApplicationController
 	# before_filter :authenticate_user!
-	# layout :admin
-	def set_use_default_layout
-    	@use_default_layout = false
-  	end
+	layout 'admin'
+	# def set_use_default_layout
+ #    	@use_default_layout = false
+ #  	end
   
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_filter :configure_permitted_parameters, if: :devise_controller?
-  before_filter :set_use_default_layout
+  # before_filter :set_use_default_layout
 
-  def set_use_default_layout
-    @use_default_layout = true
-  end
+  # def set_use_default_layout
+  #   @use_default_layout = true
+  # end
 
   def after_sign_in_path_for(resource)
     if resource.admin?
