c6cd54f4bfa80c0e8a95d4a2775f7d4f2ff514e4
vishwnath <vishwnath.yadav@ongraph.com>
2014-07-18 01:43:41 -0700

    Library category added

--- app/models/library.rb
+++ app/models/library.rb
@@ -9,6 +9,10 @@ class Library < ActiveRecord::Base
 
 	validates :title, :directions, :category, :difficulty, presence: true
 	
+	CATEGORIES = ["Agility", "BodybuildingBodyweight","Cardio","Core","Dance","Dumbbell","Endurance","Exercise Ball","Kettlebell","Martial Arts","Medicine Ball","Office","Pilates","Playground","Postnatal","Prenatal","Pull Ups","Recovery","Resistance Bands","Stairs","Stretching","TRX","Warm Up","Yoga"]
+
+	DIFFICULTIES = ["Beginner","Intermediate","Athletic","Elite"]
+
 	def build_association
 		1.times{target_muscle_groups.build if self.target_muscle_groups.empty? }
 	end
