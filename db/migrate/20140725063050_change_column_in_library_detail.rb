5a50deb0d17d3cab38e67d82fee8c9dd57c32d85
vishwnath <vishwnath.yadav@ongraph.com>
2014-07-25 08:16:35 -0700

    changes

--- db/migrate/20140725063050_change_column_in_library_detail.rb
+++
@@ -1,7 +0,0 @@
-class ChangeColumnInLibraryDetail < ActiveRecord::Migration
-  def change
-  	rename_column :library_details, :repetition, :repetitions
-  	rename_column :library_details, :is_duration, :duration
-  	rename_column :library_details, :is_tempo, :tempo
-  end
-end
