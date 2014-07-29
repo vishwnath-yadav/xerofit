9d9638d12d23979ab9048ec8302da287af91e8dd
vishwnath <vishwnath.yadav@ongraph.com>
2014-07-29 04:20:28 -0700

    admin panel modifications

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,9 +22,9 @@ class ApplicationController < ActionController::Base
       end
   end
 
-  rescue_from CanCan::AccessDenied do |exception|
-    redirect_to root_url, :alert => exception.message
-  end
+  # rescue_from CanCan::AccessDenied do |exception|
+  #   redirect_to root_url, :alert => exception.message
+  # end
 
 
   protected
