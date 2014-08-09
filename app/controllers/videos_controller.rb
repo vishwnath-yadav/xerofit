d347d96a214293845f3fe28ee7f8786ec3f41d7b
vishwnath <vishwnath.yadav@ongraph.com>
2014-08-09 18:12:17 +0530

    css changes

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -3,6 +3,7 @@ class VideosController < ApplicationController
 	
 	respond_to :json, :js
 	def create
+		binding.pry
 	  	if session[:video_id].blank?
 			@video = LibraryVideo.new(video: params[:file]) 
 	    else
@@ -12,9 +13,6 @@ class VideosController < ApplicationController
 	    if @video.save
 	  		p_video = Panda::Video.create!(source_url: @video.video.to_s, path_format: "panda_video/:video_id/:profile/:id")
 	  		@video.panda_video_id = p_video.id
-	  		if @video.library_id.present?
-	  			@video.library_id = params[:lib_id]
-	  		end
 	  		@video.save
 	    end
     	render text: "#{@video.present? ? @video.id : ""}"
