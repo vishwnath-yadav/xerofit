172fd9f437865a5fa156a10a9eebeb692162be14
Kyle Bollinger <kbollinger32@gmail.com>
2014-09-01 21:22:02 -0700

    do not need to define layout anymore

--- app/controllers/admin/workouts_controller.rb
+++ app/controllers/admin/workouts_controller.rb
@@ -1,9 +1,8 @@
 class Admin::WorkoutsController < Admin::AdminController
     before_filter :authenticate_user!
-    layout 'admin'
 
 	def index
 		@workout = Workout.all.order('created_at desc')
 	end
-	
+
 end
