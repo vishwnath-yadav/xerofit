5b974516a9866fe82ff00b930335165fab2575a6
Kyle Bollinger <kbollinger32@gmail.com>
2014-09-01 21:13:05 -0700

    remove old and unused layout

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,22 +1,21 @@
 class DashboardController < ApplicationController
 	before_filter :authenticate_user!
-	# layout 'sub_menu_layout'
-	
+
     def index
     end
-    
+
     def trainer
 		render current_user.role
 	end
-	
+
 	def create
-    omniouth = request.env["omniauth.auth"]
-    @user = User.from_omniauth(omniouth)
-    if @user.enabled
-        sign_in :user, @user
-    	redirect_to root_path, notice: 'Signed in successfully!'
-    else
-    	redirect_to new_user_session_path
+        omniouth = request.env["omniauth.auth"]
+        @user = User.from_omniauth(omniouth)
+        if @user.enabled
+            sign_in :user, @user
+        	redirect_to root_path, notice: 'Signed in successfully!'
+        else
+        	redirect_to new_user_session_path
+        end
     end
-  end
 end
