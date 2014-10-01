61101b757bf7e6baaeec2966b8d63f622e78dbe4
Vishwanath <vishwanath.yadav@ongraph.com>
2014-10-01 15:30:56 +0530

    tempo validations in workout move details saved

--- app/models/move_detail.rb
+++ app/models/move_detail.rb
@@ -1,6 +1,6 @@
 class MoveDetail < ActiveRecord::Base
 	belongs_to :move_block
-    validate :validate_workout
+    validate :validate_details
 
 	REP = (0..60).to_a
 	DURATION = ["Seconds", "Minutes", "Hours"]
@@ -8,9 +8,11 @@ class MoveDetail < ActiveRecord::Base
 	REP_TYPE = ["Repeat Until Failure","Number Range","Fixed Number"]
 	DIST_OPT = ["Miles","Feet","Meters", "Kilometers"]
 
-	def validate_workout
+	def validate_details
 		if !(repetitions or distance  or duration or weight)
 			self.errors.add(:base, "Atleast 1 of Repetitions, Weight, Distance and Duration field is required in order to save a workout")
+		elsif tempo && !(temp_lower > 0 or temp_pause > 0 or temp_lift > 0)
+			self.errors.add(:base, "Atleast 1 of Tempo's lower, pause, and lift field must be greater than 0 in order to save a workout")
 		end
 	end
 end
