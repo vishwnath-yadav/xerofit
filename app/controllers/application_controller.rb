db18181da4f59d9614514696131cf3a9aa93f757
Vishwanath <vishwanath.yadav@ongraph.com>
2014-10-07 18:04:29 +0530

    changes in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_filter :configure_permitted_parameters, if: :devise_controller?
   before_filter :check_admin_user_or_not
-  
   layout :layout_by_resource
 
   def after_sign_in_path_for(resource)
