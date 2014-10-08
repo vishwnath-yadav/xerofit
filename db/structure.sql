b12f55ed8b7ccffc84f1232eae5f627dcfdd2352
Kyle Bollinger <kbollinger32@gmail.com>
2014-10-08 12:59:06 -0700

    rework the trainer dashboard homescreen to introduce the discover app. Also convert app icons from using pngs to pure css solution

--- db/structure.sql
+++ db/structure.sql
@@ -3,7 +3,8 @@
 --
 
 SET statement_timeout = 0;
-SET client_encoding = 'LATIN1';
+SET lock_timeout = 0;
+SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 SET check_function_bodies = false;
 SET client_min_messages = warning;
