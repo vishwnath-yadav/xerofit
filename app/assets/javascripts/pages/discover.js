80e647be12d61826c0553fb01ccea19bb731f14e
Kyle Bollinger <kbollinger32@gmail.com>
2014-10-09 23:29:55 -0700

    discover move video page styles. basically finished

--- app/assets/javascripts/pages/discover.js
+++ app/assets/javascripts/pages/discover.js
@@ -9,12 +9,14 @@ $(document).ready(function() {
 
     if(scroll >= 84){
       sticky.addClass('fixed');
+      sticky.css('border-bottom','1px solid #E3E3E3');
       $('#discover-video-header .show-on-scroll').css('display','block');
       $('.discover .control_bar .control_bar_context').remove('.header-title-wrap');
       $('.discover .control_bar .header-title-wrap').appendTo('#discover-video-header .show-on-scroll');
     }
     else{
       sticky.removeClass('fixed');
+      sticky.css('border-bottom','none');
       $('#discover-video-header .show-on-scroll').css('display','none');
       $('.header-title-wrap').appendTo('.discover .control_bar .control_bar_context');
       $('#discover-video-header .show-on-scroll').remove('.header-title-wrap');
